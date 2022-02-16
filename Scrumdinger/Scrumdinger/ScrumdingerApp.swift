8c0bc4d0a1dc08ba0859719f74762c5c5e60b5ea
Abraham Abreu <slek_2001@hotmail.com>
2022-02-16 12:06:58 -0600

    First Commit

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Abraham Abreu on 16/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
