ab01d429cab4d752a070dc884265edae26341263
Abraham Abreu <slek_2001@hotmail.com>
2022-02-16 17:59:48 -0600

    Add the data binding between views

--- Scrumdinger/Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Scrumdinger/Views/DetailView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct DetailView: View {
-    let scrum: DailyScrum
+    @Binding var scrum: DailyScrum
+    @State private var data = DailyScrum.Data()
+    @State private var isPresentingEditView = false
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
@@ -41,13 +43,38 @@ struct DetailView: View {
             }
         }
         .navigationTitle(scrum.title)
+        .toolbar {
+            Button("Edit") {
+                isPresentingEditView = true
+                data = scrum.data
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationView {
+                DetailEditView(data: $data)
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                                scrum.update(from: data)
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            DetailView(scrum: DailyScrum.sampleData[1])
+            DetailView(scrum: .constant(DailyScrum.sampleData[0]))
         }
     }
 }
---
+++ Scrumdinger/Scrumdinger/Views/ThemePicker.swift
@@ -0,0 +1,26 @@
+//
+//  ThemePickerView.swift
+//  Scrumdinger
+//
+//  Created by Abraham Abreu on 16/02/22.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}
